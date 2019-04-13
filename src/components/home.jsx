0d1ec112aec96230b846b67d6dc045bc5642cae4
Jasper Swallen <swallen@usc.edu>
2019-04-13 14:46:28 -0700

    directions list

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,15 +5,14 @@ import { places, GeoCoder, Map, InfoWindow, Marker, GoogleApiWrapper, MapDirecti
 const google = window.google;
 const $ = window.$;
 let geocoder = new google.maps.Geocoder();
-<<<<<<< HEAD
-var directionsService = new google.maps.DirectionsService;
-var directionsDisplay = new google.maps.DirectionsRenderer;
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
 
 var dest;
 var initial;
-=======
-const GOOGLE_MAPS_APIKEY = 'AIzaSyBU0az33dTdv2Z655ieXC4NVk7Bm_N9d2g';
->>>>>>> af43414a4240c971fe1bff83583c87502028ca07
+
+//var map = new google.maps.Map(document.getElementById('map'), );
+
 
 class App extends React.Component {
 
@@ -35,12 +34,12 @@ class App extends React.Component {
 
   initializeMap() {
     function autocompleteStartLocation() {
-       dest = document.getElementById('destField');
+      dest = document.getElementById('destField');
       var autocomplete = new google.maps.places.Autocomplete(dest);
     }
 
     function autocompleteDestination() {
-       initial = document.getElementById('initField');
+      initial = document.getElementById('initField');
       var autocomplete = new google.maps.places.Autocomplete(initial);
     }
 
@@ -59,49 +58,43 @@ class App extends React.Component {
 
   calculateAndDisplayRoute(directionsService, directionsDisplay) {
     //geocode
-        // Geocode the address
+    // Geocode the address
+    geocoder.geocode({
+      'address': dest.value
+    }, function (results, status) {
+      if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
+        //define results
+        let destLat = results[0].geometry.location.lat();
+        let destLong = results[0].geometry.location.lng();
+        //recurse with init
         geocoder.geocode({
-          'address': dest.value
+          'address': initial.value
         }, function (results, status) {
           if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
             //define results
-            let destLat = results[0].geometry.location.lat();
-            let destLong = results[0].geometry.location.lng();
-            //recurse with init
-            geocoder.geocode({
-              'address': initial.value
-            }, function (results, status) {
-              if (status === google.maps.GeocoderStatus.OK && results.length > 0) {
-                //define results
-                let startLat = results[0].geometry.location.lat();
-                let startLong = results[0].geometry.location.lng();
+            let startLat = results[0].geometry.location.lat();
+            let startLong = results[0].geometry.location.lng();
 
-                console.log(startLat, startLong, destLat, destLong);
+            console.log(startLat, startLong, destLat, destLong);
 
-                //here is where we could use the lat/long to get a route
-                directionsService.route({
-                  origin: {lat: startLat, lng: startLong},
-                  destination: {lat: destLat, lng: destLong},
-                  travelMode: 'DRIVING'
-                }, function(response, status) {
-                  if (status === 'OK') {
-                    console.log('directions okay', response);
-                    directionsDisplay.setDirections(response);
-                    directionsDisplay.setMap(document.getElementById('map'));
+            //here is where we could use the lat/long to get a route
+            directionsService.route({
+              origin: { lat: startLat, lng: startLong },
+              destination: { lat: destLat, lng: destLong },
+              travelMode: 'DRIVING'
+            }, function (response, status) {
+              if (status === 'OK') {
+                console.log('directions okay', response);
+                directionsDisplay.setDirections(response);
+                directionsDisplay.setMap(document.getElementById('map'));
+                directionsDisplay.setPanel(document.getElementById('directionsPanel'));
 
-                  } else {
-                    window.alert('Directions request failed due to ' + status);
-                  }
-                });
-
-
-                // show an error if it's not
               } else {
-                alert("Error with destination field! Please try again and make sure the address is valid!");
-                console.log(status);
-                console.log(results);
+                window.alert('Directions request failed due to ' + status);
               }
             });
+
+
             // show an error if it's not
           } else {
             alert("Error with destination field! Please try again and make sure the address is valid!");
@@ -109,6 +102,13 @@ class App extends React.Component {
             console.log(results);
           }
         });
+        // show an error if it's not
+      } else {
+        alert("Error with destination field! Please try again and make sure the address is valid!");
+        console.log(status);
+        console.log(results);
+      }
+    });
     //end geocode
   }
 
@@ -120,28 +120,29 @@ class App extends React.Component {
           <input className="searchBar" id="initField" type="text" />
           <label htmlFor="destField">Destination</label>
           <input className="searchBar" id="destField" type="text" />
-          <button className="btn btn-default" id="search" onClick={() => {this.calculateAndDisplayRoute(directionsService,directionsDisplay)}}>Check Route!</button>
+          <button className="btn btn-default" id="search" onClick={() => { this.calculateAndDisplayRoute(directionsService, directionsDisplay) }}>Check Route!</button>
           <span className="spinner">
             <span className="double-bounce1"></span>
             <span className="double-bounce2"></span>
           </span>
         </div>
-        
-        <Map
+
+        {/*<Map
           centerAroundCurrentLocation
           className="map"
           id="map"
           google={google}
           style={{ height: '100%', position: 'relative', width: '100%' }}
-<<<<<<< HEAD
-          zoom={14}
-        />
-
-=======
           zoom={14}>
-                    {/*<MapDirections strokeWidth={3} strokeColor="hotpink" apikey={GOOGLE_MAPS_APIKEY}/>*/}
-        </Map> 
->>>>>>> af43414a4240c971fe1bff83583c87502028ca07
+
+        </Map>*/}
+        <div id="directionsPanel" style={{ "height": "100%", "width": "100%" }}>
+
+        </div>
+        <div id="map" style={{ "height": "100%", "width": "100%" }}>
+
+        </div>
+
       </div>
 
     )
