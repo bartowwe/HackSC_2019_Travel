6e2b2ce8d38cfba4e9f769201eb3063bbc562b41
Bartow <heunderhill@gmail.com>
2019-04-12 22:18:45 -0700

    Fullstack Template
    Fullstack template uploaded for prototyping

---
+++ Server/controller.js
@@ -0,0 +1,9 @@
+
+const blogController = {
+    //this api is only using the get functions so we will ignore the rest of CRUD
+    get: (req,res) =>{
+
+    },
+};
+
+module.exports = { blogController };
